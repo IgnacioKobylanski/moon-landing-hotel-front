9d392bfb044defe5b3cc8d5268ad631019d6199d
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-12-11 19:19:27 -0300

    roomPage component created

---
+++ src/pages/RoomPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RoomPage = ({ match }) => {
+    const roomid = match.params.id;
+
+    return (
+        <div className="room-details">
+             <h2></h2>
+        </div>
+       
+    );
+
+};
