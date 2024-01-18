2658936d239351d9fa80282d76bbaa881bd86494
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-18 19:46:57 -0300

    favicon added, header have mayor changes in the structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import '../styles/Header.css';
-import Login from './Login'; // Asegúrate de proporcionar la ruta correcta al componente Login
+import Login from './Login';
 
 const Header = ({ onLogin }) => {
     return (
         <header>
-            <nav className='header-nav'>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-            </nav>
-            <h1 className="title-header">Moon Landing Hotel</h1>
-            <Login onLogin={onLogin} />
+            
         </header>
     );
 };
