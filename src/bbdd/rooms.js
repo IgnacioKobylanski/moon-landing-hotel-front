8bab0180a129227fb3abe8d91f809313f96ec5bb
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-02-29 19:00:34 -0300

    new rooms added

--- src/bbdd/rooms.js
+++ src/bbdd/rooms.js
@@ -22,6 +22,12 @@ const rooms = [
         name: 'Old World',
         price: '$750',
         imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+        id: 5,
+        name: 'Classic',
+        price: '$450',
+        imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
       }
 ];
 
