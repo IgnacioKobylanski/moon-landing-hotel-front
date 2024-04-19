17a1fcb6209b130751e65f566acaa86b5cfd08c5
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-04-19 14:35:40 -0300

    checkout page has new content

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -5,6 +5,7 @@ function CheckoutPage(){
         <div className="checkout-main">
         <h2>Your reservations</h2>
         <p>Here you can check all the information regarding your reservations</p>
+        <section className="reservations"></section>
         </div>
     );
 }
