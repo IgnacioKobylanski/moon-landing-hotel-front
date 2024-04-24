63c824830bd59088f553ea11c0a1c0a2cfe798f1
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-04-24 11:36:17 -0300

    some users added to users.js

--- src/bbdd/users.js
+++ src/bbdd/users.js
@@ -1,9 +1,14 @@
 const users = [
     {
         id:1,
-        email:'',
-        password:''
-}
+        email:'pepe@mail.com',
+        password:'pepe1234'
+    },
+    {
+        id:2,
+        email:'carlo@mail.com',
+        password:'carlo1234'
+    }
 ]
 
 export default users;
