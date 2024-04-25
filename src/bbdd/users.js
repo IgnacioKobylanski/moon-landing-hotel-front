c307ee0ae5ab3e69d6b89f8076e250a517f56274
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-04-25 15:57:49 -0300

    admin boolean atribute added to users

--- src/bbdd/users.js
+++ src/bbdd/users.js
@@ -1,16 +1,19 @@
 const users = [
     {
         id:1,
+        admin:false,
         email:'pepe@mail.com',
         password:'pepe1234'
     },
     {
         id:2,
+        admin:false,
         email:'carlo@mail.com',
         password:'carlo1234'
     },
     {
         id:3,
+        admin:false,
         email:'pedro@mail.com',
         password:'pedro1234'
     }
