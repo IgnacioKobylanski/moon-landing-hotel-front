85f7415e0d104c3feac411856a3324a170b23179
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-04-26 16:32:27 -0300

    create user page created

---
+++ src/pages/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateUser = () => {
+    return(
+        <div className="create-user-main">
+            <p>Welcome</p>
+        </div>
+    );
+};
+
+export default CreateUser;
