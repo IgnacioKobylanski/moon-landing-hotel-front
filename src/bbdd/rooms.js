a4116b63e111fdb65d240a7958b2272312fb4a29
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-05-23 16:56:39 -0300

    more rooms removed from rooms.js

--- src/bbdd/rooms.js
+++ src/bbdd/rooms.js
@@ -70,14 +70,7 @@ const rooms = [
         name: 'Classic',
         price: '$550',
         imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-      },
-      {
-        id: 9,
-        name: 'Classic',
-        price: '$550',
-        imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
       }
-      
 
 ];
 
