c70bcd87068ca6e4a73071c137eca75b87b2ae75
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-05-27 14:08:18 -0300

    minor changes in rooms.js

--- src/bbdd/rooms.js
+++ src/bbdd/rooms.js
@@ -52,12 +52,6 @@ const rooms = [
         name: 'Classic',
         price: '$550',
         imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-      },
-      {
-        id: 7,
-        name: 'Classic',
-        price: '$550',
-        imageUrl: 'https://images.pexels.com/photos/15011341/pexels-photo-15011341/free-photo-of-kocatas-de-seis-sentidos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
       }
 
 ];
