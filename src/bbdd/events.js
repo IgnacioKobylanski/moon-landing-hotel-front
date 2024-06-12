20d25faed92ded1169449bc6315d8650d5f0ae41
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-06-12 11:24:25 -0300

    structure change in events,js

--- src/bbdd/events.js
+++ src/bbdd/events.js
@@ -28,16 +28,6 @@ const events = [
         dateD: 15,
         dateM: 'May',
         img: 'https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1://images.pexels.com/photos/1677710/pexels-photo-1677710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    {
-        id: 4,
-        name: 'Ted Talk',
-        description: 'Ted Talk is coming to town and is waiting for you!',
-        text: 'Do you want to waste your time listening to weird people?, look no further.',
-        price: 550,
-        dateD: 15,
-        dateM: 'May',
-        img: 'https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1://images.pexels.com/photos/1677710/pexels-photo-1677710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     }
     
 ];
