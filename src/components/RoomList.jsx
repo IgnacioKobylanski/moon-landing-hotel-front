c93875e768317d95a55101a4526eca385d0288da
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-07-12 17:00:13 -0300

    minor fox in RoomList.jsx and RoomList.css

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -7,7 +7,7 @@ import rooms from '../bbdd/rooms';
 
 const RoomList = () => {
     return (
-        <div className="roomList-main-container">
+        <div className="roomList-main">
             <h2>Available Rooms</h2>
             {rooms.map(room => (
                 <RoomCard key={room.id} roomInfo={room} />
