917e50603a8681ffd4ff141103d4f6651d15b651
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-10-18 10:29:45 -0300

    Restaurant.jsx created

---
+++ src/pages/Restaurant.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Restaurant = () => {
+    return(
+        <div className="resto-main">
+        <h2> Tasty Food</h2>
+        </div>
+    )
+}
+
+export default Restaurant;
