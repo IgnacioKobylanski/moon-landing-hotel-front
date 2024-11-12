dbfba7283b656bcf343ab2b5b266d32cd2e308b5
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-11-12 16:15:05 -0300

    minor error in App.js fixed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { useState } from 'react';  // Importa useState para manejar el estado de usuario
-import Login from './components/Login';  // Asegúrate de que importas el componente Login
+import { useState } from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -13,19 +12,15 @@ const App = () => {
 
     // Función para manejar el login y actualizar el estado con los datos del usuario
     const handleLogin = (userData) => {
-        setUserData(userData);  // Guarda la información del usuario en el estado
-        console.log("Usuario logueado:", userData);  // Esto te ayudará a depurar
+        setUserData(userData); 
+        console.log("Usuario logueado:", userData);  
     };
 
     return (
         <Router>
             <div className="App">
-                {/* Pasamos la función handleLogin al componente Login */}
                 <Header onLogin={handleLogin} />
-                {/* Aquí pasamos el userData a Main (por si lo necesitas en ese componente) */}
                 <Main userData={userData} />
-                {/* Mostrar Login solo si no hay un usuario logueado */}
-                {!userData && <Login onLogin={handleLogin} />} 
             </div>
             <Footer />
         </Router>
